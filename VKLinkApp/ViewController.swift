ff7e3e0f82fd4b0fa490bb2902c1904b40bb6462
vishelpodishat <alishsaideshov@gmail.com>
2024-03-28 13:39:01 +0500

    Initial Commit

---
+++ VKLinkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKLinkApp
+//
+//  Created by Alisher Saideshov on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
